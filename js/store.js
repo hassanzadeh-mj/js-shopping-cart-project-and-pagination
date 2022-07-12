98e340f96db0a101fdbea185e0b0c1b248419560
Mojtabahassanzadeh <mj.hassanzadeh1995@gmail.com>
2022-07-12 14:58:15 +0430

    add : js code

--- js/store.js
+++ js/store.js
@@ -1,12 +1,12 @@
 let allProducts = [
-    { id: 1, title: 'item 1', price: 150000, img: 'Images/Album 1.png' },
-    { id: 2, title: 'item 2', price: 300000, img: 'Images/Album 2.png' },
-    { id: 3, title: 'item 3', price: 40000, img: 'Images/Album 3.png' },
-    { id: 4, title: 'item 4', price:60000, img: 'Images/Album 4.png' },
-    { id: 5, title: 'Coffee', price: 200000, img: 'Images/Cofee.png' },
-    { id: 6, title: 'Shirt', price: 300000, img: 'Images/Shirt.png' },
-    { id: 7, title: 'Coffee', price: 980000, img: 'Images/Cofee.png' },
-    { id: 8, title: 'Shirt', price:50000, img: 'Images/Shirt.png' },
+    { id: 1, title: 'محصول 1', price: 150000, img: 'Images/Album 1.png' ,count: 1},
+    { id: 2, title: 'محصول 2', price: 300000, img: 'Images/Album 2.png',count: 1 },
+    { id: 3, title: 'محصول 3', price: 40000, img: 'Images/Album 3.png',count: 1 },
+    { id: 4, title: 'محصول 4', price:60000, img: 'Images/Album 4.png' ,count: 1},
+    { id: 5, title: 'محصول 5', price: 200000, img: 'Images/Cofee.png',count: 1 },
+    { id: 6, title: 'محصول 6', price: 300000, img: 'Images/Shirt.png',count: 1 },
+    { id: 7, title: 'محصول 7', price: 980000, img: 'Images/Cofee.png' ,count: 1},
+    { id: 8, title: 'محصول 8', price:50000, img: 'Images/Shirt.png',count: 1 },
 ]
 let userBasket=[]
 let $ = document
@@ -14,6 +14,7 @@ let userListContainer = $.querySelector('.shop-items')
 let paginationContainer = $.querySelector('#pagination')
 const bastekProductsContainer = $.querySelector('.cart-items')
 const removeAllProductsBtn = $.querySelector('.remove')
+const cartTotalPriceElem = $.querySelector('.cart-total-price')
 
 
 let currentPage = 1
@@ -55,18 +56,25 @@ function displayUesrsList (allUesrsArray, usersContainer, rowsCount, currentPage
         productDetailsContainer.append(productPriceSpan, prodcutAddButton)
     
         productContainer.append(productTitleSpan, productImageElem, productDetailsContainer)
-    
+        
         userListContainer.append(productContainer)
     
     })
 
 }
 const addProductToBasketArray =(productId)=>{
-    let mainProduct =allProducts.find(p=>p.id===productId)
+    let mainProduct =allProducts.find(p=> p.id===productId)
+    let someElem= userBasket.some(product=>product.id===productId)
+   if (!someElem) {
     userBasket.push(mainProduct)
+}else{
+    mainProduct.count=mainProduct.count+1
+}
     basketProductsGenerator(userBasket)
+    calcTotalPrice(userBasket)
 }
 const basketProductsGenerator=(userBasketArray)=>{
+    
     bastekProductsContainer.innerHTML = ''
     userBasketArray.forEach ( (product)=> {
 
@@ -91,19 +99,22 @@ const basketProductsGenerator=(userBasketArray)=>{
         let basketProductPriceSpan = $.createElement('span')
         basketProductPriceSpan.className = 'cart-price cart-column'
         basketProductPriceSpan.innerHTML = product.price
-
         let basketProductInputsContainer = $.createElement('div')
         basketProductInputsContainer.className = 'cart-quantity cart-column'
 
         let basketProductInput = $.createElement('input')
         basketProductInput.className = 'cart-quantity-input'
-        basketProductInput.value = '1'
+        basketProductInput.value = product.count
         basketProductInput.setAttribute('type', 'number')
-
+        basketProductInput.addEventListener("change",()=>{
+            updateProductCount(product.id,basketProductInput.value)
+        })
         let basketProductRemoveBtn = $.createElement('button')
         basketProductRemoveBtn.className = 'btn btn-danger'
-        basketProductRemoveBtn.innerHTML = 'Remove'
+        basketProductRemoveBtn.innerHTML = 'حذف'
         basketProductRemoveBtn.addEventListener("click",()=>{
+            product.count=1
+            calcTotalPrice(userBasketArray)
             removeProductFromBasket(product.id)
         })
         basketProductInputsContainer.append(basketProductInput, basketProductRemoveBtn)
@@ -145,6 +156,14 @@ function paginationButtonGenerator (page, allUesrsArray) {
 
     return button
 }
+const calcTotalPrice =(userBasketArray)=>{
+    let totalPriceValue=0
+    cartTotalPriceElem.innerHTML=0
+    userBasketArray.forEach((product)=>{
+        totalPriceValue+=product.count*product.price
+    })
+    cartTotalPriceElem.innerHTML=totalPriceValue+" تومان"
+}
 const removeProductFromBasket=(productId)=>{
     userBasket=userBasket.filter(p=>p.id!==productId)
     basketProductsGenerator(userBasket)
@@ -155,6 +174,14 @@ removeAllProductsBtn.addEventListener("click",()=>{
     basketProductsGenerator(userBasket)
 
 })
+const updateProductCount=(productId,newCount)=>{
+    userBasket.forEach(product=>{
+        if(product.id===productId){
+            product.count=newCount
+        }
+    })
+    calcTotalPrice (userBasket)
+}
 displayUesrsList(allProducts, userListContainer, rowsCount, currentPage)
 setupPagination(allProducts, paginationContainer, rowsCount)
 
